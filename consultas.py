bc57a10de919ba481d89c31342b1e143af38fbee
MarcioGBecker <mrcbecker1@gmail.com>
2024-04-03 18:53:25 -0300

    Finalizado o código para as pesquisas!

--- consultas.py
+++ consultas.py
@@ -1,10 +1,23 @@
 import json
-
-lista = []
+from datetime import date
 
 with open('campeoes-br.json', 'r', encoding='utf-8') as f:
     tabelas = json.load(f)
 
-ano = tabelas[1]['ano']
+today = date.today()
+consulta = int(input('Informe o ano que deseja consultar:(YYYY) '))
 
-print (ano)
+
+while consulta >= today.year:
+    print ("O campeonato não foi concluído ou realizado!")
+    print (f"No momento temos o campeonato até o ano de {today.year - 1}.")
+    consulta = int(input('Informe o ano que deseja consultar:(YYYY) '))
+
+for ano in tabelas:
+    
+    if str(consulta) == ano['ano']:
+        print (
+            f"1° colocado: {ano['vencedor']}\n"
+            f"2° colocado: {ano['vice']}\n"
+            f"3° colocado: {ano['bronze']}"
+            )
